da858fcd5ecc201d8c74bc9c49a520d0b1c35d70
euduwawala <ErangaU@99x.lk>
2020-02-10 05:18:17 +0530

    Updates

---
+++ precache-manifest.58ec7fbe76f3ef41b1207e34833c854d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028b12d8519f3e7b0d0875de9c68dc1c",
+    "url": "/imagepwa/index.html"
+  },
+  {
+    "revision": "5e03674b577644922502",
+    "url": "/imagepwa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c05e5ff5723575b0c359",
+    "url": "/imagepwa/static/js/2.52c23128.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/imagepwa/static/js/2.52c23128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e03674b577644922502",
+    "url": "/imagepwa/static/js/main.f1e0ed92.chunk.js"
+  },
+  {
+    "revision": "493ec364bebc506477e9",
+    "url": "/imagepwa/static/js/runtime-main.c7dfb572.js"
+  }
+]);
